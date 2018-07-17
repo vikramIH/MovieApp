d78523b528278167072066b47f9a037f71fe19b1
vikramIH <vrihanji@gmail.com>
2018-07-17 11:52:27 +0530

    Added

--- src/test/java/com/stackroute/movieApp/MovieAppApplicationTests.java
+++ src/test/java/com/stackroute/movieApp/MovieAppApplicationTests.java
@@ -11,9 +11,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MovieAppApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+//	@Test
+//	public void contextLoads() {
+//	}
 
 //	@Test
 //    public void fakeTest(){
--- src/test/java/com/stackroute/movieApp/repositories/FakeIntegrationTestIT.java
+++ src/test/java/com/stackroute/movieApp/repositories/FakeIntegrationTestIT.java
@@ -1,14 +1,17 @@
 package com.stackroute.movieApp.repositories;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
 public class FakeIntegrationTestIT {
 
-  @Test
-  public void fakeTest(){
-      assertTrue(true);
-  }
-
+    @Test
+    public void fakeTest(){
+        assertTrue(true);
+    }
 }
