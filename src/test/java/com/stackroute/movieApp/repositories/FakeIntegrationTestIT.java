85ea08fa27d9ef915bbedf256087d7a40a04af8f
vikramIH <vrihanji@gmail.com>
2018-07-17 15:29:17 +0530

    qwerty

--- src/test/java/com/stackroute/movieApp/repositories/FakeIntegrationTestIT.java
+++ src/test/java/com/stackroute/movieApp/repositories/FakeIntegrationTestIT.java
@@ -3,11 +3,13 @@ package com.stackroute.movieApp.repositories;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class FakeIntegrationTestIT {
 
     @Test
