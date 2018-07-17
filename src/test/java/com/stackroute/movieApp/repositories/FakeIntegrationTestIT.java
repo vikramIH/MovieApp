41cdaba7dbc6b8539350598b64d381b5106990a1
vikramIH <vrihanji@gmail.com>
2018-07-17 11:44:33 +0530

    Added test case

---
+++ src/test/java/com/stackroute/movieApp/repositories/FakeIntegrationTestIT.java
@@ -0,0 +1,14 @@
+package com.stackroute.movieApp.repositories;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FakeIntegrationTestIT {
+
+  @Test
+  public void fakeTest(){
+      assertTrue(true);
+  }
+
+}
